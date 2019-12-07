17ab872b73e3045fe2e34821ce8461b5a1ff0962
vikentorpierre <vikentorpierre@gmail.com>
2019-12-06 18:30:41 -0800

    Updates

---
+++ precache-manifest.c55b5e679bd3e7e75ab533ff15ea3616.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea574c53154a43d4b523cbf1bf858ebf",
+    "url": "/simpleflix/index.html"
+  },
+  {
+    "revision": "a6320551475d990986fc",
+    "url": "/simpleflix/static/css/main.2d810b7b.chunk.css"
+  },
+  {
+    "revision": "e3039bce204c56b38e0c",
+    "url": "/simpleflix/static/js/2.7894c935.chunk.js"
+  },
+  {
+    "revision": "a6320551475d990986fc",
+    "url": "/simpleflix/static/js/main.914eb5f2.chunk.js"
+  },
+  {
+    "revision": "d9e358a58d86265a15a5",
+    "url": "/simpleflix/static/js/runtime-main.5e11ca37.js"
+  },
+  {
+    "revision": "4b514fbf346f8363eed03955504c0a93",
+    "url": "/simpleflix/static/media/Twitter.4b514fbf.png"
+  },
+  {
+    "revision": "353f215b10e6610edb12595308bd7c5d",
+    "url": "/simpleflix/static/media/image3.353f215b.png"
+  },
+  {
+    "revision": "f778aa42b2ccacfbf60f03726aa83d30",
+    "url": "/simpleflix/static/media/insta.f778aa42.png"
+  },
+  {
+    "revision": "95b93e1dfdaf021634013a37aca66a7e",
+    "url": "/simpleflix/static/media/logo1.95b93e1d.png"
+  },
+  {
+    "revision": "d6873c34759f79b87d52f39dbd86b407",
+    "url": "/simpleflix/static/media/mail.d6873c34.png"
+  },
+  {
+    "revision": "cfc77a246299b4c282baf9472470d3fd",
+    "url": "/simpleflix/static/media/me.cfc77a24.jpg"
+  },
+  {
+    "revision": "9c4efddb0805f901dbdbef4cd3278d61",
+    "url": "/simpleflix/static/media/vid2.9c4efddb.PNG"
+  },
+  {
+    "revision": "0c07600c7b47e703eab943c806f0bfcd",
+    "url": "/simpleflix/static/media/vid3.0c07600c.PNG"
+  },
+  {
+    "revision": "61ee32a53dccf238db85fb7a65cf7cca",
+    "url": "/simpleflix/static/media/vid4.61ee32a5.PNG"
+  },
+  {
+    "revision": "26421c85c033eb8b71fb016d47d4141f",
+    "url": "/simpleflix/static/media/vid5.26421c85.PNG"
+  }
+]);
